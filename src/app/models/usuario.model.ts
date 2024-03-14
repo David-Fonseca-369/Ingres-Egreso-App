c8406c0d97780f22d24d88ff055c2018c9ab99b3
Fonseca <fonsecasoundcruz@gmail.com>
2024-03-13 23:06:27 -0600

    Implemetamos el auth con ngrx

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,4 +1,8 @@
 export class User {
+  //Hacemos des estructuración para obtener ciertas propiedades
+  static fromFirebase({ email, uid, nombre }) {
+    return new User(uid, nombre, email);
+  }
   constructor(
     public uid: string,
     public nombre: string,
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {
   Auth,
+  Unsubscribe,
   authState,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -11,27 +12,43 @@ import { map } from 'rxjs/operators';
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
 
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import * as authActions from '../auth/auth.actions';
+import { Firestore, collection, doc, getDocs, onSnapshot, query, where } from '@angular/fire/firestore';
+import { User } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  userUnsubscribe!: Unsubscribe;
+
   constructor(
     private auth: Auth,
-    private router: Router
+    private router: Router,
+    private store: Store<AppState>,
+    private firestore: Firestore
   ) {}
 
   //Se encargará de avisarnos cuando suceda algún cambio con la autenticación
   initAuthListener() {
-    authState(this.auth).subscribe((fUser) => {
-      console.log(fUser);
-      console.log(fUser?.uid);
-      console.log(fUser?.email);
-      console.log(fUser?.displayName);
-    });
+    authState(this.auth).subscribe( async fUser => {
+      if (fUser) {
+        const userRef = collection(this.firestore, 'user')
+        const q = query(userRef, where("uid", "==", fUser.uid));
+        const querySnapshot = (await getDocs(q))
+        querySnapshot.forEach((doc: any) => {
+          this.store.dispatch(authActions.setUser({user: doc.data()}))
+        })
+      } else {
+        this.userUnsubscribe ? this.userUnsubscribe() : null;
+        this.store.dispatch(authActions.unSetUser());
+      }
+    })
   }
   createUser(name: string, email: string, password: string) {
-    return createUserWithEmailAndPassword(this.auth, email, password)
+    return createUserWithEmailAndPassword(this.auth, email, password);
   }
 
   loginUser(email: string, password: string) {
